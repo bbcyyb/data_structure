bd3d80ee0e7681cbf2c8130a294a9ab429350c6e
Kevin.Y.Yu <bbcyyb@gmail.com>
2017-10-12 22:48:07 +0800

    Implementls insert and

--- list/linked_list.py
+++ list/linked_list.py
@@ -3,79 +3,98 @@
 
 class LinkedList(object):
     def __init__(self):
+        self._head = LinkedNode()
+        self._tail = LinkedNode()
+        self._head.prev = self._tail
+        self._tail.next = self._head
 
-        self._head = None
+    def add_first(self, data):
+        """
+        Adds a new node containing the specified value
+        at the start of the LinkedList
+        """
+        node = LinkedNode(data)
+        self._tail.next.prev = node
+        node.next = self._tail.next
+        node.prev = self._tail
+        self._tail.next = node
 
-    def append(self, value):
+    def add_last(self, data):
+        """
+        Adds a new node containing the specified value
+        at the end of the LinkedList
+        """
+        node = LinkedNode(data)
+        self._head.prev.next = node
+        node.prev = self._head.prev
+        node.next = self._head
+        self._head.prev = node
 
-        node = self.LinkedNode(value)
-        self._head = node
+    def contains(self, data):
+        """
+        Determines whether a value is in the LinkedList
+        """
+        pass
+
+    def find(self):
+        """
+        Finds the first node that contains the specified value
+        """
+        pass
+
+    def find_last(self):
+        """
+        Finds the last node that contains the specified value
+        """
+        pass
+
+    def remove(self, data):
+        """
+        Removes the first occurrence of the specified value
+        from the LinkedList
+        """
+        pass
+
+    def remove_first(self):
+        """
+        Removes the node at the start of the LinkedList
+        """
+        if self._tail.next == self._head or self._head.prev == self._tail:
+            raise Exception("list is empty.")
+        removed_node = self._tail.next
+        self._tail.next = self._tail.next.next
+        self._tail.next.prev = self._tail
+        removed_node.prev = None
+        removed_node.next = None
+
+    def remove_last(self):
+        """
+        Removes the node at the end of the LinkedList
+        """
+        if self._tail.next == self._head or self._head.prev == self._tail:
+            raise Exception("list is empty.")
+        removed_node = self._head.prev
+        self._head.prev = self._head.prev.prev
+        self._head.prev.next = self._head
+        removed_node.prev = None
+        removed_node.next = None
+
+
+    def remove_all(self):
+        """
+        Removes all nodes from the LinkedList
+        """
+        pass
+
+    def count(self):
+        """
+        Gets the number of nodes actually contained in the LinkedList
+        """
+        pass
 
     class LinkedNode(object):
-        def __init__(self, data):
+        def __init__(self, data=None):
 
             self.data = data
             self.next = None
-
-        def add_first(self, data):
-            """
-            Adds a new node containing the specified value
-            at the start of the LinkedList
-            """
-            pass
-
-        def add_last(self, data):
-            """
-            Adds a new node containing the specified value
-            at the end of the LinkedList
-            """
-            pass
-
-        def contains(self, data):
-            """
-            Determines whether a value is in the LinkedList
-            """
-            pass
-
-        def find(self):
-            """
-            Finds the first node that contains the specified value
-            """
-            pass
-
-        def find_last(self):
-            """
-            Finds the last node that contains the specified value
-            """
-            pass
-
-        def remove(self, data):
-            """
-            Removes the first occurrence of the specified value
-            from the LinkedList
-            """
-            pass
-
-        def remove_first(self):
-            """
-            Removes the node at the start of the LinkedList
-            """
-            pass
-
-        def remove_last(self):
-            """
-            Removes the node at the end of the LinkedList
-            """
-            pass
-
-        def remove_all(self):
-            """
-            Removes all nodes from the LinkedList
-            """
-            pass
-
-        def count(self):
-            """
-            Gets the number of nodes actually contained in the LinkedList
-            """
-            pass
+            self.prev = None
