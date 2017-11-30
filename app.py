585abe96de4d4d87b55439fde0e454077c66d9b2
Kevin.Y.Yu <bbcyyb@gmail.com>
2017-11-30 22:56:35 +0800

    Implement happy number

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 # coding: utf-8
 from tree.tire_tree import TireTree
+from happy_number import HappyNumber
 
 if __name__ == "__main__":
+
+    happy_lst = []
+    unhappy_lst = []
+    entity = HappyNumber()
+    for num in range(1,1000):
+        if entity.happy(num):
+            happy_lst.append(num)
+        else:
+            unhappy_lst.append(num)
+    print 'happy list is ', happy_lst
+    print '============================='
+    print 'unhappy list is ', unhappy_lst
+    """
     tire = TireTree()
     tire.insert('hello')
     tire.insert('world')
@@ -10,7 +24,6 @@ if __name__ == "__main__":
     tire.insert('head')
     lst = tire.get_all_words()
     print lst
-    """
     tire.output()
     tire.delete('wow')
     tire.output()
