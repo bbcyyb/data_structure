d02c8a2e7ce3ddf996abb4e3e8002dccffd4b12e
Kevin.Y.Yu <bbcyyb@gmail.com>
2017-11-30 22:57:42 +0800

    Implement happy number

---
+++ happy_number.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+
+class HappyNumber:
+
+    def __init__(self):
+        self._happyNum = {}
+        self._unhappyNum = {}
+
+    def happy(self, num):
+        print num
+        current = num
+        passed_array = {}
+
+        if self._happyNum.has_key(num):
+            return True
+        elif self._unhappyNum.has_key(num):
+            return False
+
+        while(True):
+            split_num = current
+            new_num = 0
+            digit_array = []
+            while(split_num):
+                digit_array.append(split_num % 10)
+                split_num = split_num / 10
+            for digit in digit_array:
+                new_num = new_num + (digit * digit)
+            if new_num == 1:
+                passed_array[current] = True
+                for key in passed_array.keys():
+                    self._happyNum[key] = True
+                return True
+            elif passed_array.has_key(current):
+                for key in passed_array.keys():
+                    self._unhappyNum[key] = True
+                return False
+            else:
+                print current
+                passed_array[current] = True
+                current = new_num
+
+
